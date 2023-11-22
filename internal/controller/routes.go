a5e28108be3dba399eac10bd3e35554abeb97c27
sthapa <sureshthapa409@gmail.com>
2023-11-22 17:48:27 +0545

    Added cors

--- internal/controller/routes.go
+++ internal/controller/routes.go
@@ -23,6 +23,7 @@ const (
 )
 
 func (ctl *Controller) Routes() {
+	ctl.Router.Use(CORSMiddleware())
 	ctl.Router.GET("/", home)
 	ctl.publicRoutes()
 	ctl.SetRoutes("/payment", "PAYMENT_SERVER_URL")
@@ -136,5 +137,19 @@ func copyHeader(dst, src http.Header) {
 		for _, v := range vv {
 			dst.Add(k, v)
 		}
+	}
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-agent-code")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
 	}
 }
